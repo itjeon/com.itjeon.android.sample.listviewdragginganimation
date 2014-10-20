d7f47f87e9f7a4139fd0c81da19611603db5fa63
Jeon In-Taek <itjeon@gmail.com>
2014-10-20 11:36:15 +0900

    Project Init

---
+++ gen/com/itjeon/android/sample/listviewdragginganimation/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.itjeon.android.sample.listviewdragginganimation;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
